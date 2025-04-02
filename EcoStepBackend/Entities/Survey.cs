01230e376cf657fba70e6159821c54816e4b0edf
Alexander Iarmoshenko <a2398253@gmail.com>
2025-04-02 19:43:58 +0500

    Add DateTime entity to Survey class

--- EcoStepBackend/Entities/Survey.cs
+++ EcoStepBackend/Entities/Survey.cs
@@ -4,6 +4,7 @@ public class Survey
 {
     public long Id { get; set; }
     public long UserId { get; set; }
+    public DateTime CompletedAt { get; set; }
     public FoodData FoodData { get; set; }
     public WasteData WasteData { get; set; }
     public TransportData TransportData { get; set; }
