d91c0d204f41ac3b19fb71ecfe26f6e353cff30e
Alexander Iarmoshenko <a2398253@gmail.com>
2025-04-11 12:42:03 +0500

    Add User eco flags

--- EcoStepBackend/EcoStepBackend/Models/User.cs
+++ EcoStepBackend/EcoStepBackend/Models/User.cs
@@ -8,4 +8,25 @@ public class User
     public string PasswordHash { get; set; } 
     public Household Household { get; set; } 
     public ICollection<Survey> Surveys { get; set; }
+    
+    public bool IsFoodMeatOk { get; set; }
+    public bool IsFoodPlantOk { get; set; }
+    public bool IsWaterOk { get; set; }
+    public bool IsElectricityOk { get; set; }
+    public bool IsCarOk { get; set; }
+    public bool IsPublicTransportOk { get; set; }
+    public bool IsAirplaneOk { get; set; }
+    
+    public bool IsFoodWasteOk { get; set; }
+    public bool IsOtherWasteOk { get; set; }
+    
+    public bool IsPlasticWasteOk { get; set; }
+    public bool IsGlassWasteOk { get; set; }
+    public bool IsPaperWasteOk { get; set; }
+    public bool IsMetalWasteOk { get; set; }
+    
+    public bool IsPlasticRecycledOk { get; set; }
+    public bool IsGlassRecycledOk { get; set; }
+    public bool IsPaperRecycledOk { get; set; }
+    public bool IsMetalRecycledOk { get; set; }
 }
