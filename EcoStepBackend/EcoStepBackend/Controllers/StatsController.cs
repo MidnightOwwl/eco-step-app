07ff1205cb86328a9dbe8c301cc50299e9813e59
Alexander Iarmoshenko <a2398253@gmail.com>
2025-04-11 12:14:30 +0500

    Prepare StatsController.GetLastSurvey

--- EcoStepBackend/EcoStepBackend/Controllers/StatsController.cs
+++ EcoStepBackend/EcoStepBackend/Controllers/StatsController.cs
@@ -4,12 +4,12 @@ using Microsoft.EntityFrameworkCore;
 namespace EcoStepBackend.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[controller]/{userId:long}")]
 public class StatsController(AppDbContext db) : ControllerBase
 {
     private readonly AppDbContext _db = db;
     
-    [HttpGet("{userId:long}")]
+    [HttpGet("")]
     public IActionResult GetStats(long userId)
     {
         var user = _db.Users
@@ -20,5 +20,25 @@ public class StatsController(AppDbContext db) : ControllerBase
             return NotFound();
 
         return Ok(user.Surveys);
+    }
+    
+    [HttpGet("last-survey")]
+    public IActionResult GetLastSurvey(long userId)
+    {
+        var user = _db.Users
+            .Include(u => u.Surveys)
+            .FirstOrDefault(u => u.Id == userId);
+        
+        if (user is null)
+            return NotFound();
+
+        var lastSurvey = user.Surveys
+            .OrderByDescending(s => s.CompletedAt)
+            .FirstOrDefault();
+        
+        if (lastSurvey is null)
+            return NotFound();
+        
+        return Ok(lastSurvey);
     }
 }
